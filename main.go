e39553574cb17956efa119ee8e809ee1304f5a45
JesusC-XR <jcienfuegos@extremereach.com>
2025-03-10 21:18:37 -0700

    Added cache functionality

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	pokeClient := pokedexapi.NewClient(5 * time.Second)
+	pokeClient := pokedexapi.NewClient(5*time.Second, time.Minute*5)
 
 	cfg := &config{
 		pokeapiClient: pokeClient,
