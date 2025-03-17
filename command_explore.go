993f1a5aff401ea6d5862bf18e812c5a2ebd8010
JesusC-XR <jcienfuegos@extremereach.com>
2025-03-17 11:06:12 -0700

    Fixed Explore command

--- command_explore.go
+++ command_explore.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 )
 
 func commandExplore(cfg *config, area_name string) error {
-	location_url := *cfg.nextLocationsURL + area_name
+	parsedURL, err := url.Parse(*cfg.nextLocationsURL)
+	if err != nil {
+		return err
+	}
+
+	defaultURL := parsedURL.Scheme + "://" + parsedURL.Hostname() + parsedURL.Path
+	location_url := defaultURL + "/" + area_name
+
 	pokemonResp, err := cfg.pokeapiClient.ListPokemon(&location_url)
 	if err != nil {
 		return err
 	}
 
+	fmt.Printf("Exploring %v...\n", area_name)
+	fmt.Println("Found Pokemon:")
+
 	for _, poke := range pokemonResp.PokemonEncounters {
-		fmt.Println(poke.Pokemon.Name)
+		fmt.Printf("- %v\n", poke.Pokemon.Name)
 	}
 
 	return nil
